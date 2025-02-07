f6fea7f18b3385cb055d6cb10cfeaf39aaa25f0f
Liam Or <liamdev656@gmail.com>
2025-02-08 00:17:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public")); // Static files from the "public" folder.
+
+// Define a route
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get("/views", (req, res) => {
+    res.render("about.ejs");
+});
+
+// start a server on port 3000
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
